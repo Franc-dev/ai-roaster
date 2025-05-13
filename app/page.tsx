bdd8a77fb890d9e605a5e7c11bfc4425e1228ef3
Franc-dev <francismwanik254@gmail.com>
2025-05-13 17:30:24 +0300

    ~ ai roaster & praiser

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ChatInterface from '@/components/ChatInterface';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen py-10">
+    <main className="flex flex-col items-center justify-center min-h-screen pb-10">
       <ChatInterface />
     </main>
   );
